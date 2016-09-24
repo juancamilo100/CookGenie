adc85abe49cbf5496e1220ed54bd1ec724464115
Alexander Perea-Artunduag <Alexander@Alexanders-MacBook-Pro-4.local>
2016-09-24 13:00:14 -0400

    testing alexa

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname + '/index.html'));
+  // res.render('index', { title: 'Express' });
 });
+
+router.get('/alexaTest', function(req, res, next) {
+  res.send({texToSpeech: "Hello world"});
+  // res.render('index', { title: 'Express' });
+});
+
 
 module.exports = router;
