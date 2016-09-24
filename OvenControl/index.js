c8c6cfd780d9c30315072f4df3d3b72822657312
LauraArtunduaga <laura.artunduaga@ge.com>
2016-09-24 17:37:15 -0400

    adding bacis oven control

---
+++ OvenControl/index.js
@@ -0,0 +1,19 @@
+var greenBean = require("green-bean");
+
+greenBean.connect("range", function(range) {
+    // range.upperOven.cookMode.read(function(value) {
+    //     console.log("upper oven cook mode is:", value);
+    // });
+
+  //   range.upperOven.remoteEnable.read(function(value) {
+  //      console.log("upper oven remote enable is:", value);
+  //  });
+
+    // range.upperOven.remoteEnable.write(1);
+    // range.upperOven.cookMode.write({
+    //     mode: 18,
+    //     cookTemperature: 350,
+    //     cookHours: 1,
+    //     cookMinutes: 0
+    // });
+});
