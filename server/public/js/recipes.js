c95962cfa2305b6137897493ae44c8e739179bae
Alexander Perea-Artunduag <Alexander@Alexanders-MacBook-Pro-4.local>
2016-09-25 11:41:20 -0400

    displaying current step in the webpage

---
+++ server/public/js/recipes.js
@@ -0,0 +1,29 @@
+var recipes = [
+  {
+    name: "pumpkin pie",
+    steps: [
+      ["2 eggs plus the yolk of a third egg",
+      "1/2 cup packed dark brown sugar",
+      "1/3 cup white sugar"],
+      "%%%%425",
+      "Beat the eggs in a large bowl. Mix in the brown sugar, white sugar, salt, spices—cinnamon, ground ginger, nutmeg, ground cloves, cardamom, and lemon zest.",
+      "Mix in the pumpkin purée. Stir in the cream. Beat together until everything is well mixed.",
+      "&&&&425;2",
+      "&&&&350;3"
+    ]
+  },
+  {
+    name: "beef brisket",
+    steps: [
+      ["3 to 4 pounds of a brisket cut of beef",
+      "3/4 cup barbeque sauce",
+      "1/4 cup soy sauce",
+      "1 cup of water"],
+      "%%%%300",
+      "In a bowl, mix together the barbeque sauce, soy sauce, and water.",
+      "Place the brisket roast on a large piece of aluminum foil. Spread the BBQ sauce mixture generously over meat. Wrap the brisket in aluminum foil and place it in a roasting pan inside the oven.",
+      "&&&&300;60",
+      "Remove from oven and let rest in the foil for 30 minutes before serving."
+    ]
+  }
+];
