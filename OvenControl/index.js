7395a620d21d52a0a3f4b997aa6c57007bf83b73
Alexander Perea-Artunduag <Alexander@Alexanders-MacBook-Pro-4.local>
2016-09-25 09:52:02 -0400

    serail port server working with particle and alexa

--- OvenControl/index.js
+++ OvenControl/index.js
@@ -1,27 +1,73 @@
 var greenBean = require("green-bean");
+var recipeIndex  = 1;
+var currentStep= 0;
+var serial = require('serialport');
+var fork = require('child_process').fork;
 
-greenBean.connect("range", function(range) {
-    // range.upperOven.cookMode.read(function(value) {
-    //     console.log("upper oven cook mode is:", value);
-    // });
 
-  //   range.upperOven.remoteEnable.read(function(value) {
-  //      console.log("upper oven remote enable is:", value);
-  //  });
-
-    // range.upperOven.remoteEnable.write(1);
-    // range.upperOven.cookMode.write({
-    //     mode: 18,
-    //     cookTemperature: 450,
-    //     cookHours: 1,
-    //     cookMinutes: 0
-    // });
-
-    // range.upperOven.remoteEnable.write(1);
-    // range.upperOven.cookMode.write({
-    //     mode: 18,
-    //     cookTemperature: 350,
-    //     cookHours: 1,
-    //     cookMinutes: 0
-    // });
+var port = new serial('/dev/tty.usbmodem1421', {
+	baudRate: 9600,
+	parser: serial.parsers.readline('\n')
 });
+
+
+port.on('error', function(err) {
+	console.log('Error: ', err.message);
+});
+
+
+port.on('data', function(data) {
+  console.log('got it:'+data);
+  if(data.trim().toLowerCase()=="something"){
+    // currentStep=23;
+    // port.write(recipeIndex+''+currentStep);
+		// try{
+			sendCommandToOven(450, 0, 30);
+			port.write(recipeIndex+''+currentStep);
+		// }catch(err){console.log('error');}
+
+
+  }else if(data.trim().toLowerCase().indexOf("startrecipe")> -1){
+			recipeIndex = data.trim().split(';')[1];
+			currentStep =0;
+      port.write(recipeIndex+''+currentStep);
+  }else if(data.trim().toLowerCase()=="nextstep"){
+		currentStep+=1;
+      port.write(recipeIndex+''+currentStep);
+  }else if(data.trim().toLowerCase().indexOf("preheatoven")>-1){
+		 var temperature = data.trim().split(';')[1];
+		console.log('preheating: '+temperature);
+		currentStep+=1;
+  	port.write(recipeIndex+''+currentStep);
+	 	sendCommandToOven(temperature, 0, 0);
+  }else if(data.trim().toLowerCase().indexOf("ovenprofile")>-1){
+		 var temperature = data.trim().split(';')[1];
+		 var minutes = data.trim().split(';')[2];
+		console.log('cooking temp: '+temperature+', cooking time:'+minutes);
+		currentStep+=1;
+  	port.write(recipeIndex+''+currentStep);
+	 	sendCommandToOven(temperature, 0, 1);
+  }
+	else if(data.trim().toLowerCase()=="current"){
+      port.write(recipeIndex+''+currentStep);
+  }else if(data.trim().toLowerCase()=="reset"){
+      currentStep = 23;
+      port.write(recipeIndex+''+currentStep);
+  }else if(data.trim().toLowerCase().indexOf("repeat")> -1){
+      port.write(recipeIndex+''+currentStep);
+  }
+
+});
+function sendCommandToOven(temp, hours, minutes){
+	var exec = require('child_process').exec;
+	exec('node ovenPreheat.js '+temp+' '+minutes, function(error, stdout, stderr) {
+	    // console.log('stdout: ' + stdout);
+	    // console.log('stderr: ' + stderr);
+	    if (error !== null) {
+	        // console.log('exec error: ' + error);
+	    }
+	});
+
+
+}
+// sendCommandToOven(450);
